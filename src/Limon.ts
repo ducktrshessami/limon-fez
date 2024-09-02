c73dbb8da8cdcd1939a25446661161a40139fc4f
ducktrshessami <cal.tf2.3@gmail.com>
2024-09-02 09:16:45 -0700

    feat: init

--- src/Limon.ts
+++ src/Limon.ts
@@ -1,6 +1,19 @@
-import { Entry } from "node-cmudict";
+import { Entry, getDict } from "node-cmudict";
 import Singleton from "./Singleton";
 
 export default class Limon extends Singleton {
-    private cache: Map<string, Set<Entry>> = new Map();
+    private _dict: Map<string, Entry> | null = null;
+    public readonly cache: Map<string, Set<Entry>> = new Map();
+
+    public get dict() {
+        return this._dict;
+    }
+
+    /**
+     * Initialize the instance with a cmudict dictionary. Overwrites the current dictionary if it exists.
+     * @param dict The dictionary to use. Defaults to getting a new cmudict dictionary.
+     */
+    public init(dict?: Map<string, Entry>): void {
+        this._dict = dict ?? getDict();
+    }
 }
