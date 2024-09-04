4718d3d952119420ae9b3d24c8fb180b1a1475cd
ducktrshessami <cal.tf2.3@gmail.com>
2024-09-04 09:55:15 -0700

    fix(Cache): filter return type

--- src/Cache.ts
+++ src/Cache.ts
@@ -8,8 +8,8 @@ export default class Cache<T> extends Set<T> {
         return false;
     }
 
-    filter(predicate: (value: T) => unknown): Set<T> {
-        const result = new Set<T>();
+    filter(predicate: (value: T) => unknown): Cache<T> {
+        const result = new Cache<T>();
         for (const item of this) {
             if (predicate(item)) {
                 result.add(item);
