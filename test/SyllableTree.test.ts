71e2f9386602c89298e03ca02fe720a661f56221
ducktrshessami <cal.tf2.3@gmail.com>
2024-10-01 13:08:35 -0700

    chore: test entries

--- test/SyllableTree.test.ts
+++ test/SyllableTree.test.ts
@@ -1,1 +1,20 @@
-import { } from "../dist/index";
+import { Entry, getDict } from "node-cmudict";
+
+function ensureEntry(dict: Map<string, Entry>, name: string): Entry {
+    const entry = dict.get(name);
+    if (!entry) {
+        throw new Error(`No entry for '${name}' in dictionary`);
+    }
+    return entry;
+}
+
+describe("SyllableTree", function () {
+    let testEntry: Entry;
+    let nestEntry: Entry;
+
+    before(function () {
+        const dict = getDict();
+        testEntry = ensureEntry(dict, "testing");
+        nestEntry = ensureEntry(dict, "nesting");
+    });
+});
